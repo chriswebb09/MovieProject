fc819c0e8df9726032573e6c180582973d963dab
chriswebb09 <christopher.webb@flatironschool.com>
2017-03-21 20:36:19 -0400

    Initial commit

---
+++ SearchMovie/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SearchMovie
+//
+//  Created by Christopher Webb-Orenstein on 3/21/17.
+//  Copyright © 2017 Christopher Webb-Orenstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
