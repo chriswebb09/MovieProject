fe0a6d351b9242e7e7c5af61c4f448e34b775b66
chriswebb09 <christopher.webb@flatironschool.com>
2017-03-28 22:29:23 -0400

    moved error extension to NSError+Extension.swift file

--- SearchMovie/MTAPIClient.swift
+++ SearchMovie/MTAPIClient.swift
@@ -11,12 +11,6 @@ import UIKit
 typealias JSON = [String : Any]
 fileprivate let imageCache = NSCache<NSString, UIImage>()
 
-extension NSError {
-    static func generalParsingError(domain: String) -> Error {
-        return NSError(domain: domain, code: 400, userInfo: [NSLocalizedDescriptionKey : NSLocalizedString("Error retrieving data", comment: "General Parsing Error Description")])
-    }
-}
-
 class MTAPIClient {
     
     //MARK: - Public
